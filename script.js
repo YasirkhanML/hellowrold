8c81cc9b50a2d454822c5a7aaf3f4c1713fb3bb9
YasirkhanML <yasir@aptechnorth.edu.pk>
2025-01-18 13:25:34 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// let x = 'abdullah';
+
+// x = 7;
+//  let x1 = 'abdullah'
+
+//  x = 123;
+//  z = true; 
+
+//  let e = 3;
+//   e = 5;
+
+//  y = '123',
+
+//  y = "123",
+//  123 == '123'
+
+//  document.write(y)
+//  document.write(typeof e)
+
+// document.write(x,"<br>", x1)
+
+// document.write(name1)
+
+// console.log(x)
+// alert(x)
